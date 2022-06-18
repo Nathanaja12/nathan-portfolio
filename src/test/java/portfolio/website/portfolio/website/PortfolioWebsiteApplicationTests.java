19732fee378d1b703769c7b19fed713428f63f70
nathanael.pardosi <nathanpardosi141@gmail.com>
2022-06-18 12:27:03 +0700

    initiate

---
+++ src/test/java/portfolio/website/portfolio/website/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.website.portfolio.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
