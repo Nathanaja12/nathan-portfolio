fded99dd8db8316fa4a89d75c9ee4f2c66856fe8
nathanael.pardosi <nathanpardosi141@gmail.com>
2022-06-19 14:50:54 +0700

    template

---
+++ src/main/java/portfolio/portfolionathan/PortfolioNathanApplication.java
@@ -0,0 +1,13 @@
+package portfolio.portfolionathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioNathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioNathanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portfolio/portfolionathan/PortfolioNathanApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.portfolionathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioNathanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
