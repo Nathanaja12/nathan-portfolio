4355e2f24d547d4de7a717248fd3e6655f0c3e61
nathanael.pardosi <nathanpardosi141@gmail.com>
2022-06-19 20:31:17 +0700

    gatau

--- src/main/resources/static/assets/theme/js/theme.js
+++ src/main/resources/static/assets/theme/js/theme.js
@@ -166,7 +166,7 @@
   //Typed Animation
   function initTyped() {
     $("#typed").typed({
-      strings: ["A Designer", "A Freelancer", "A Marketer", "A Developer"],
+      strings: ["A Learner", "An Explorer"],
       // typing speed
       typeSpeed: 300,
       // time before typing starts
